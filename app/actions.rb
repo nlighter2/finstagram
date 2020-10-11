f211d0aefedab0cc3530cda46b33264ad223dd81
nlighter2 <nlighter2@gmail.com>
2020-10-11 18:42:52 +0000

    loops

--- app/actions.rb
+++ app/actions.rb
@@ -47,5 +47,8 @@ get '/' do
     }
     
     [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin].to_s
+
+    @finstagram_posts = [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin]
+    
     erb(:index)
 end
